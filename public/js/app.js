4ea280d3dcb7492217afca4c9b571a0bd97087fa
Aralya Phinith <aphinith84@gmail.com>
2016-10-07 12:56:30 -0700

    get list of names to show on page

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,28 @@
-var header = document.getElementsByTagName("h1");
-console.log('this is header: ', header[0]);
-function test() {
-  alert('This is the header: ', header[0]);
-}
+document.addEventListener("DOMContentLoaded", function() {
+
+  var allUsers = ['Nick', 'jake', 'RAY', 'Kate', 'Nick',  'Jeremy', 'nick', 'AMOL', 'rAY', 'VIANNEY', 'Shilpika', 'nick', 'THOMAS', 'tom', 'james', 'JERM', 'amOl', 'kate', 'SCOTT', 'Jenifer', 'bill', 'jenny', 'STEVEN'];
+
+  
+  for (var i = 0; i < allUsers.length; i++) {
+    var ul = document.getElementsByClassName("all-usernames")[0];
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode(allUsers[i]));
+    li.setAttribute("class", "username");
+    ul.appendChild(li);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
