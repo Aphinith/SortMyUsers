57c77638414ebbcaf4744a56d765a0572238656d
Aralya Phinith <aphinith84@gmail.com>
2016-10-07 17:21:19 -0700

    lowercase sorting button functionality now complete

--- public/js/app.js
+++ public/js/app.js
@@ -143,8 +143,54 @@ document.addEventListener("DOMContentLoaded", function() {
 
   //create function to sort by all lowercase first
   var sortByLowercase = function() {
+    //clear the ul tag
     var ul = document.getElementsByClassName("all-usernames")[0];
     ul.innerHTML = '';
+
+    userId = 0;
+
+    //create variable to hold list of users with corresponding Id's
+    var usernameWithId = [];
+
+    //create class to instantiate username with id
+    var UserObject = function(id, username) {
+      this.id = id;
+      this.username = username;
+    }
+
+    //iterate through sorted users to make instantiation of UserObjects
+    for (var i = 0; i < allUsersSorted.length; i++) {
+      userId++;
+      //push new instantiations into usernameWithId array
+      usernameWithId.push(new UserObject(userId, allUsersSorted[i]));
+    }
+
+    //create two new arrays, one to hold names that start with uppercase and one to hold names that start with a lowercase
+    var uppercaseNames = [];
+    var lowercaseNames = [];
+
+    //iterat through usernameWithId array
+    for (var i =0; i < usernameWithId.length; i++) {
+      //now we can separate the names that begin with uppercase and lowercase by pushing them to their corresponding arrays
+      if (usernameWithId[i]['username'][0] === usernameWithId[i]['username'][0].toUpperCase()) {
+        uppercaseNames.push(usernameWithId[i])
+      } else {
+        lowercaseNames.push(usernameWithId[i])
+      }
+    }
+    //create array to combine newly ordered names with matching ids, starting with names that begin a capital letter first, then all names sorted beginning with lower case letters
+    var joinedNames = lowercaseNames.concat(uppercaseNames);
+
+    //iterate through list and populate names on DOM
+    for (var i = 0; i < joinedNames.length; i++) {
+      ul = document.getElementsByClassName("all-usernames")[0];
+      var li = document.createElement("li");
+      li.appendChild(document.createTextNode(joinedNames[i]['username']));
+      li.setAttribute("class", "username");
+      li.setAttribute("data-key", joinedNames[i]['username']);
+      li.setAttribute("data-id", joinedNames[i]['id']);
+      ul.appendChild(li);
+    }
   }
 
 
