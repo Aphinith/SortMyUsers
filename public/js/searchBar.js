37229093846ace3f3ccafa334d40115b8d024e92
Aralya Phinith <aphinith84@gmail.com>
2016-10-09 16:38:00 -0700

    search bar not case sensitive and background-color remains

--- public/js/searchBar.js
+++ public/js/searchBar.js
@@ -1,12 +1,12 @@
 var searchUsers = function() {
   var matches = 0;
-  var usernameSearch = document.getElementsByClassName("search-input")[0].value;
+  var usernameSearch = document.getElementsByClassName("search-input")[0].value.toLowerCase();
   var userList = document.getElementsByClassName("username");
   for (var i = 0; i < userList.length; i++) {
-    userList[i].style.backgroundColor = "white"
+    userList[i].style.backgroundColor = "azure"
     var username = userList[i].getAttribute("data-username");
     console.log('username:', username);
-    if (usernameSearch === username) {
+    if (usernameSearch === username.toLowerCase()) {
       matches++;
       userList[i].style.backgroundColor = "yellow";
     }
